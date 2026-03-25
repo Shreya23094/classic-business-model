1b9e662df47b206e14c86dade13a75a829837b77
Pragya Saraswat <saraswatpragya2006@gmail.com>
2026-03-25 16:24:48 +0530

    Added entity classes for customers and employees

---
+++ src/main/java/com/businessmodel/entity/Employee.java
@@ -0,0 +1,98 @@
+package com.businessmodel.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    private Integer employeeNumber;
+    private String lastName;
+    private String firstName;
+    private String extension;
+    private String email;
+    @ManyToOne
+    @JoinColumn(name = "officeCode")
+    private Office office;
+    @ManyToOne
+    @JoinColumn(name = "reportsTo")
+    private Employee manager;
+    private String jobTitle;
+
+    public Employee() {}
+
+    public Employee(Integer employeeNumber, String lastName, String firstName, String extension, String email, Office office, Employee manager, String jobTitle) {
+        this.employeeNumber = employeeNumber;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.extension = extension;
+        this.email = email;
+        this.office = office;
+        this.manager = manager;
+        this.jobTitle = jobTitle;
+    }
+
+    public Integer getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(Integer employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Office getOffice() {
+        return office;
+    }
+
+    public void setOffice(Office office) {
+        this.office = office;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}
