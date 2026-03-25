222572d14c78ba3454f2f71d73ac5073d48589f4
Pragya Saraswat <saraswatpragya2006@gmail.com>
2026-03-25 14:18:00 +0530

    Initial project structure

---
+++ src/main/java/com/businessmodel/ClassicBusinessModelApplication.java
@@ -0,0 +1,13 @@
+package com.businessmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicBusinessModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassicBusinessModelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/businessmodel/ClassicBusinessModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.businessmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicBusinessModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
