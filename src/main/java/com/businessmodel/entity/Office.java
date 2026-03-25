22a86d5a554133bd4de2d7ee408f15eafd8cbc15
Singh <workwithnaina.ha12@gmail.com>
2026-03-25 16:53:15 +0530

    Added Office entity

---
+++ src/main/java/com/businessmodel/entity/Office.java
@@ -0,0 +1,120 @@
+package com.businessmodel.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "offices")
+public class Office {
+
+    @Id
+    private String officeCode;
+    private String city;
+    private String phone;
+    private String addressLine1;
+    private String addressLine2;
+    private String state;
+    private String country;
+    private String postalCode;
+    private String territory;
+
+    @OneToMany(mappedBy = "office")
+    private List<Employee> employees;
+
+    // Default Constructor
+    public Office() {}
+
+    // Parameterized Constructor
+    public Office(String officeCode, String city, String phone, String addressLine1, String addressLine2, String state, String country, String postalCode, String territory) {
+        this.officeCode = officeCode;
+        this.city = city;
+        this.phone = phone;
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.state = state;
+        this.country = country;
+        this.postalCode = postalCode;
+        this.territory = territory;
+    }
+
+    // Getters and Setters
+    public String getOfficeCode() {
+        return officeCode;
+    }
+
+    public void setOfficeCode(String officeCode) {
+        this.officeCode = officeCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getTerritory() {
+        return territory;
+    }
+
+    public void setTerritory(String territory) {
+        this.territory = territory;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
